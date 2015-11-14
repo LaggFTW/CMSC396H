0e55c5c65213956e765e16a27f3aec2882d7d269
Matt Myers <m@myersmatthew.com>
2015-11-13 19:37:00 -0500

    added genre feature and template for additional features

---
+++ add_to_ARFF.py
@@ -0,0 +1,65 @@
+﻿import os
+import getopt
+import sys
+import h5py
+
+matt_path = 'C:\\Users\\Matt\\Documents\\-5- Fall 2015\\CMSC396h project\\MillionSongSubset\\data\\'
+# Takes in an existing ARFF file, finds the matching HDF5 file for each instance, does additional analysis to generate more features, and creates a new ARFF file with the additional features appended to the header and to each instance
+
+def main(argv):
+    def usage():
+        print "[.ARFF file]"
+    try:
+        opts, args = getopt(argv, "", [""]) 
+        ifile = open(argv[0], 'rb')             
+    except GetoptError:           
+        usage()                         
+        sys.exit(2)  
+    for opt, arg in opts:
+        pass
+
+    output = open('modified_data.arff', 'wb')
+    additional_headers = []
+    headers_done = False
+
+    ###### Change this value to the path to your MillionSongSubset/data folder
+    os.chdir(matt_path)
+    files = {}
+    for dir, paths, names in os.walk("."):
+        for name in names:
+            if name.endswith('.h5'):
+                files[fname.split('.')[1].split('\\')[-1]] = os.path.join(dir, name)
+
+
+    for line in ifile:
+        if not headers_done and line[0] == '@':
+            continue
+        else:
+            if not headers_done:
+                # We just finished the header lines, so we add the new headers
+                headers_done = True
+                output.write('\n'.join(additional_headers))
+            elif len(line.split(',')) < 2:
+                # We're at the lines between headers and data, so copy them over
+                output.write('%s\n' % line)            
+            else:
+                # Adding data to instances
+                id = line.split(',')[0]
+                f = h5py.File(files[id], 'r')
+                
+                additional_data = []
+                #Create additional data here
+                
+                #
+
+                output.write(line)
+                output.write(',')
+                output.write(','.join(additional_data))
+                output.write('\n')
+
+    output.close()
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- h5py_to_ARFF.py
+++ h5py_to_ARFF.py
@@ -9,42 +9,53 @@ from datetime import datetime
 from csv import writer
 
 matt_path = 'C:\\Users\\Matt\\Documents\\-5- Fall 2015\\CMSC396h project\\MillionSongSubset\\data\\'
+matt_genres_path = 'C:\\Users\\Matt\\Documents\\-5- Fall 2015\\CMSC396h project'
 
 def get_stats(myList):
     a = numpy.array(myList, dtype = 'f')
+    g_opt = False
+    ofile = 'data.arff'
     return [numpy.mean(a), numpy.median(a), numpy.var(a), numpy.min(a), numpy.max(a), stats.skew(a), stats.kurtosis(a)]
 def generate_headers(s):
     return ["%s-mean" % s,"%s-med" % s,"%s-var" % s,
             "%s-min" % s,"%s-max" % s,"%s-skw" % s,"%s-krt" % s ]
 def main(argv):
     def usage():
-        print "[]"
+        print "[-g for Allmusic genres]"
     try:
-        opts, args = getopt(argv, "-c", [""])                         
+        opts, args = getopt(argv, "-g", [""])                         
     except GetoptError:           
         usage()                         
         sys.exit(2)  
     for opt, arg in opts:
-        if opt == "-c":
-            os.chdir(arg)    
+        if opt == "-g":
+            g_opt = True
+            ofile = 'genres_data.arff'
 
     print datetime.now(), "Start"
 
     #### Make this the path to (and including) your own /data folder
+    
     os.chdir(matt_path)
 
     #Initializes headers and writes header information to .arff file
     header_categories =['pitches0','pitches1','pitches2','pitches3','pitches4','pitches5','pitches6','pitches7','pitches8','pitches9','pitches10','pitches11', 'timbre0', 'timbre1','timbre2','timbre3','timbre4','timbre5','timbre6','timbre7','timbre8','timbre9','timbre10','timbre11', 'loudmax']
     headers = []
+    genres_h = ['Pop_Rock', 'Electronic', 'Rap', 'Jazz', 'Latin', 'RnB', 'International', 'Country', 'Religious', 'Reggae', 'Blues', 'Vocal', 'Folk', 'New', 'Comedy_Spoken', 'Stage', 'Easy_Listening', 'Avant_Garde', 'Classical', 'Children', 'Holiday']
+    genres_header = "\n@ATTRIBUTE genre {%s}\n" % ",".join(genres_h)
+
     for h in header_categories:
         temp_h = generate_headers(h)
         for h1 in temp_h:
             headers.append("@ATTRIBUTE %s NUMERIC" % h1)
-    output = open('data.arff', 'w')
+    output = open(ofile, 'wb')
     output.write('@RELATION song\r\n')
+    output.write('@ATTRIBUTE ID string\r\n')
     output.write("\n".join(headers))
-    output.write("\r\n@DATA\n")
+    if g_opt:
+        output.write(genres_header)
 
+    output.write("\r\n@DATA\n")
 
 
     #Collects files in the current directory
@@ -58,13 +69,31 @@ def main(argv):
     print len(files)
     print len(headers)
     counter = 0
+
+    # Opens the Allmusic_Genre_Subset.txt file to select only files that have genres associated with them
+    # NOTE: will need to update path if the file isn't here
+    if g_opt:
+        genres = {}
+        valid_files = []
+        os.chdir(matt_genres_path)
+        gfile = open('Allmusic_Genre_Subset.txt', 'rb')
+        for line in gfile:
+            l = line.split()
+            genres[l[0]] = l[1]
+        for fname in files:
+            if genres.has_key(fname.split('.')[1].split('\\')[-1]):
+                valid_files.append(fname)
+        files = valid_files
+        os.chdir(matt_path)
+
     for fname in files:
-        if counter % 1000 == 0:
+        if counter % 100 == 0:
             print datetime.now(), "Starting instance %d" % counter
         counter += 1
 
 
         features = []
+        features.append(fname.split('.')[1].split('\\')[-1])
         f = h5py.File(fname, 'r')
         db = f['analysis']
 
@@ -81,6 +110,9 @@ def main(argv):
             r = get_stats([x[i] for x in timbre])
             features.extend(r)
         features.extend(get_stats(loudmax))
+
+        if g_opt:
+            features.append(genres[fname.split('.')[1].split('\\')[-1]])
 
         output.write(",".join([str(i) for i in features]))
         output.write("\n")
